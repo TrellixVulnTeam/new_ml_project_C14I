bd604f0f9c522a85a5f16f695f59d68e76ccf6d7
bhimrathivyankatesh@gmail.com <bhimrathivyankatesh@gmail.com>
2022-06-18 17:07:51 +0530

    app.py restored to its original with logging

--- app.py
+++ app.py
@@ -7,13 +7,8 @@ app =Flask(__name__)
 
 @app.route("/", methods=['GET','POST'])
 def index():
-    try:
-        raise Exception("we are testing custom exception")
-    except Exception as e:
-        housing = HousingException(e, sys)
-        logging.info(housing.error_message)
-        logging.info("we are testing logging module")
-        return 'starting of machine learning project'
+    logging.info("we are testing logging module")
+    return 'starting of machine learning project'
 
 
 if __name__ == "__main__":
